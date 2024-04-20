507ba6bcd72e3180ef9b41cf78ceddd9e0837d68
Jahonsher <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>
2024-04-20 23:37:27 +0500

    as

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Header from './companents/Header'
 import Main from './companents/Main'
+import Page1 from './companents/Page1'
+import Page5075 from './companents/Page5075'
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
         <img src="../assets/image/small.svg" alt="rasm" />
         <Header/>
         <Main/>
+        <Page1/>
+        <Page5075/>
       </div>
     </div>
   )
---
+++ src/companents/Page5075/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <div>
+      <h1>Page 50-70</h1>
+    </div>
+  )
+}
+
+export default Index
