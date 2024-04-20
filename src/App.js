24544c94061528ce3d3ba79ca5f997d2d6a72597
AdhamMeyliyev <adhammeyliyev8@gmail.com>
2024-04-20 20:47:32 +0500

    mobile-starter

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="container">
+        salom
+        <img src="../assets/image/small.svg" alt="rasm" />
+      </div>
+    </div>
+  )
+}
+
+export default App
