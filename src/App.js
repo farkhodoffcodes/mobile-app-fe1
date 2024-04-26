80e45a590ce18578e3abb67f1f345dab1fff8721
Xamrayev Xasan <xamrayevxasan11@gmail.com>
2024-04-26 22:28:25 +0500

    page77-finished

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Page01 from "./components/Pages0125/Page01";
 import Page02 from "./components/Pages0125/Page02";
 import Page76 from "./components/Pages76100/Page76";
+import Page77 from "./components/Pages76100/Page77";
 import Page151 from "./components/Pages151175/Page151";
 import Page152 from "./components/Pages151175/Page152";
 import Page153 from "./components/Pages151175/Page153"
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/page01" element={<Page01 />} />
           <Route path="/page02" element={<Page02 />} />
           <Route path="/page76" element={<Page76 />} />
+          <Route path="/page77" element={<Page77 />} />
           <Route path="/page151" element={<Page151 />} />
           <Route path="/page152" element={<Page152 />} />
           <Route path="/page153" element={<Page153/>} />
