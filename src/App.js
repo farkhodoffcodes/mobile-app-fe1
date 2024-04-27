07e8aa4d7c713daa9b9b2ba303969beb1565ff10
Jahonsher <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>
2024-04-27 15:30:17 +0500

    51-61 pages finished

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Page01 from "./components/Pages0125/Page01";
 import Page02 from "./components/Pages0125/Page02";
 import Page151 from "./components/Pages151175/Page151"
+import Pages5175 from "./components/Pages5175"
 // import { BrowserRouter, Routes } from "react-router-dom";
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
         <Page01 />
         <Page02/>
         <Page151/>
+        <Pages5175/>
       </div>
     </>
   )
