3a52280e02c55e88e4bbde516492ad6310ba1872
AdhamMeyliyev <adhammeyliyev8@gmail.com>
2024-04-27 11:15:42 +0500

    ...

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Page77 from "./components/Pages76100/Page77";
 import Page151 from "./components/Pages151175/Page151";
 import Page152 from "./components/Pages151175/Page152";
 import Page153 from "./components/Pages151175/Page153"
+import Page03 from "./components/Pages0125/Page03";
 
 const App = () => {
   return (
@@ -16,6 +17,10 @@ const App = () => {
           <Route path="/" element={() => "First Page"} />
           <Route path="/page01" element={<Page01 />} />
           <Route path="/page02" element={<Page02 />} />
+          <Route path="/page03" element={<Page03 />} />
+          <Route path="/page03" element={<Page03 />} />
+          <Route path="/page76" element={<Page76 />} />
+          <Route path="/page77" element={<Page77 />} />
           <Route path="/page151" element={<Page151 />} />
           <Route path="/page152" element={<Page152 />} />
           <Route path="/page153" element={<Page153/>} />
